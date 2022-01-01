412651bb4df1c3f5e127dd7674afada079715a3b
Natatatg <natatat@gmail.com>
2021-12-31 21:53:40 -0600

    readme changes

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Favorite Space
 This is my submission for the [YearOne Holiday Dev Challenge](https://joinyearone.notion.site/YearOne-Holiday-Dev-Challenge-bf60ff1f14d548a2a488d0b2327126e7). This app displays the 20 most recent Astronomy Picture of the Day from NASA with a collapsible description/caption and ability to like images. 
 
+The app is available [online](http://natatatg.github.io/favorite-space).
+
+Built with React.js, Material-UI components, Axios, and Luxon.
+
 ### API Usage
 This app makes use of the publicly available NASA APOD (Astronomy Picture of the Day) [API](https://api.nasa.gov/).
 
--- src/components/SpaceCard.js
+++ src/components/SpaceCard.js
@@ -9,11 +9,8 @@ import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-//import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-// import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-//import MoreVertIcon from '@mui/icons-material/MoreVert';
 import RocketLaunchTwoToneIcon from '@mui/icons-material/RocketLaunchTwoTone';
 import { DateTime } from 'luxon';
 
